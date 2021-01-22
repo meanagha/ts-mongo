b70df843b5d51b1bddc1a629759b8f3992d91181
anagha <iamanaghapatil@gmail.com>
2021-01-22 18:25:34 +0530

    folder structure created for database connectivity

--- README.md
+++ README.md
@@ -4,7 +4,18 @@
 - Here I am creating apis like insert and list of books.
 - init project
 - install ts and normal js packages.
-- "build" folder is "outDir" folder in tsconfig.json
+- ======"build" folder is "outDir" folder in tsconfig.json
+- ======="build" folder contains all .js file after compile .ts files.
 - Once you compile project using,
     - npx tsc
     - npm run dev (for this have to write script in package.json)
+
+
+- Programmatically difference between ts and js
+
+1) import package
+
+- .ts 
+    - import express from 'express';
+- .js
+    - const express = require('express')
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
 import express from 'express';
-import * as bodyparser from 'body-parser';
+import bodyparser from 'body-parser';
+import { requestLoggerMiddleware } from '../middleware/middleware';
 
 const app = express();
+
 app.use(bodyparser.json());
 require('dotenv').config()
+app.use(requestLoggerMiddleware);
 
+const PORT = process.env.PORT || 9000;
+app.get('/list',(req,res)=>{
+    res.json([{id:1,description:'Buy Bead'}])
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {   
+})
+app.listen(PORT, async () => {   
     console.log(`Server is running on port ${PORT}.`);
 });
